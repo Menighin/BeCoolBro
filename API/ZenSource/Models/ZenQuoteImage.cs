53563b00086ef67dd846abff9b2f0c69eeb96907
Menighin <joao.menighin@gmail.com>
2017-06-20 21:12:46 -0300

    Drawing the author name

--- API/ZenSource/Models/ZenQuoteImage.cs
+++ API/ZenSource/Models/ZenQuoteImage.cs
@@ -19,18 +19,19 @@ namespace ZenSource.Models
 
         private List<Tuple<int, int>> _fontMaps = new List<Tuple<int, int>>()
         {
-            new Tuple<int, int>(48, 21),
-            new Tuple<int, int>(42, 18),
-            new Tuple<int, int>(36, 15),
-            new Tuple<int, int>(30, 12)
+            new Tuple<int, int>(56, 34),
+            new Tuple<int, int>(48, 29),
+            new Tuple<int, int>(42, 25),
+            new Tuple<int, int>(36, 22),
+            new Tuple<int, int>(30, 19)
         };
-
+        
         public ZenQuoteImage(ZenMessageViewModel viewModel)
         {
             _image = Image.Load(@"Resources/imgs/quote_background.png");
 
             _fonts = new FontCollection();
-            _font = _fonts.Install(@"Resources/fonts/Cousine.ttf");
+            _font = _fonts.Install(@"Resources/fonts/Cousine-Italic.ttf");
 
             DrawText(viewModel);
 
@@ -51,32 +52,36 @@ namespace ZenSource.Models
             var X_TRANSLATE = 50;
             var Y_TRANSLATE = 90;
             var LINE_HEIGHT = _fontMaps[0].Item1 - 2;
-            var MAX_WIDTH = 600;
-            var MAX_HEIGHT = 380;
+            var MAX_WIDTH = 1050;
+            var MAX_HEIGHT = 590;
 
             var words = viewModel.Message.Split(' ');
 
             // Find the right font size so it can fit on the specs above
             var fit = false;
             var fontConfigCount = 1;
-            var text = new List<String>() { "" };
+            var text = new List<String>() { "\"" };
             while (!fit && fontConfigCount < _fontMaps.Count + 1)
             {
                 var lineNumber = 0;
                 foreach (var w in words)
                 {
-                    if ((text[lineNumber].Length + w.Length) * FONT_WIDTH < MAX_WIDTH)
+                    if ((text[lineNumber].Length + w.Length) * FONT_WIDTH + X_TRANSLATE < MAX_WIDTH - X_TRANSLATE)
                     {
                         text[lineNumber] += w + " ";
                     }
                     else
                     {
-                        text.Add(w + " ");
+                        text.Add(" " + w + " ");
                         lineNumber++;
                     }
                 }
 
-                if (text.Count * LINE_HEIGHT < MAX_HEIGHT || fontConfigCount == _fontMaps.Count)
+                // Remove last space
+                text[lineNumber] = text[lineNumber].Remove(text[lineNumber].Length - 1);
+                text[lineNumber] += "\"";
+
+                if (text.Count * LINE_HEIGHT + Y_TRANSLATE < MAX_HEIGHT || fontConfigCount == _fontMaps.Count)
                 {
                     fit = true;
                 }
@@ -86,7 +91,7 @@ namespace ZenSource.Models
                     FONT_WIDTH = _fontMaps[fontConfigCount].Item2;
                     LINE_HEIGHT = _fontMaps[fontConfigCount].Item1 - 2;
                     fontConfigCount++;
-                    text = new List<String>() { "" };
+                    text = new List<String>() { "\"" };
                 }
 
             }
@@ -94,13 +99,27 @@ namespace ZenSource.Models
             // Draw the text
             var font = new Font(_font, FONT_SIZE);
             var y = 1;
+            var maxX = 0;
             foreach (var l in text)
             {
                 _image.DrawText(l, font, Rgba32.White, new Vector2(X_TRANSLATE, Y_TRANSLATE + (y++) * LINE_HEIGHT));
+                if (l.Length * FONT_WIDTH + X_TRANSLATE > maxX)
+                    maxX = l.Length * FONT_WIDTH + X_TRANSLATE;
             }
 
-            // TODO: Draw author name
-            
+            // Drawing Author name
+
+            // Finding the position
+            var authorFontWidth = 19;
+            var authorX = maxX - viewModel.Author.Length * authorFontWidth;
+
+            while (authorX + viewModel.Author.Length * authorFontWidth > MAX_WIDTH - X_TRANSLATE)
+                authorX -= 10;
+
+            // Defining the Font
+            var fontAuthor = new Font(_fonts.Install(@"Resources/fonts/Cousine.ttf"), 30);
+
+            _image.DrawText(viewModel.Author, fontAuthor, Rgba32.White, new Vector2(authorX, Y_TRANSLATE + (y++) * LINE_HEIGHT));
         }
 
     }
